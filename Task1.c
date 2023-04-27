cb820d70db236a17ae3bcea653ab9e0882bc4a78
cartermusee <cartermusee@gmail.com>
2023-04-27 07:56:47 -0700

    task1

--- Task1.c
+++ Task1.c
@@ -8,7 +8,7 @@ void execmd(char **av);
  */
 int main(int ac, char **av)
 {
-	char *prompt = "(simpleshell)$";
+	char *prompt = "$";
 	ssize_t totalread;
 	char *buffer = NULL, *buffer2 = NULL;
 	size_t n = 0;
