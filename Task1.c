8f5ba5ac95b841cffa9528609f6858f7ca9211be
cartermusee <cartermusee@gmail.com>
2023-04-27 03:09:57 -0700

    changed some functions and some lines

--- Task1.c
+++ Task1.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+void execmd(char **av)
 /**
  * main - Creates a simple shell
  * @ac: number of arguments
@@ -10,14 +10,12 @@ int main(int ac, char **av)
 {
 	char *prompt = "(simpleshell)$";
 	ssize_t totalread;
-	char *buffer = NULL;
+	char *buffer = NULL, *buffer2 = NULL;
 	size_t n = 0;
 	char *split;
 	char *del = " \n";
 	int splitcount = 0;
 	int i;
-	pid_t child;
-	int status;
 	(void)ac;
 
 	while (1)
@@ -27,57 +25,70 @@ int main(int ac, char **av)
 		if (totalread == -1)
 		{
 			perror("exiting\n");
-			return (-1);
-		}
-		buffer[strcspn(buffer, "\n")]= 0;
-		split = strtok(buffer, del);
-
-		while (!split)
-		{
-			continue;
-		}
-		splitcount = 1;
-		while (strtok(NULL, del))
-		{
-			splitcount++;
-		}
-		av = malloc((splitcount + 1) * sizeof(char *));
-
-		while (!av)
-		{
-			perror("memoery not alloacated");
 			exit(EXIT_FAILURE);
 		}
-
-		i = 0;
-		av[i++] = split;
-
-		while ((split = strtok(NULL, del)))
+		if (strcmp(buffer, "exit\n") == 0)
 		{
-			av[i++] = split;
+			exit(EXIT_SUCCESS);
+		}
+		buffer2 = malloc(sizeof(char) * totalread);
+		if (buffer2 == NULL)
+		{
+			perror("failed to allocate memory");
+			exit(EXIT_FAILURE);
+		}
+		strcpy(buffer2, buffer);
+		split = strtok(buffer, del);
+		while (split != NULL)
+		{
+			splintcount++;
+			split = strtok(NULL, del);
+		}
+		splitcount ++;
+		av = malloc(splitcount * sizeof(char *));
+		split = strtok(buffer2, del);
+
+		for (i = 0; split != NULL; i++)
+		{
+			av[i] = malloc(sizeof(char) * strlen(split));
+			if (av[i] == NULL)
+			{
+				perror("memoery failed");
+				exit(EXIT_FAILURE);
+			}
+			strcpy(av[i], split);
+			split = strtok(NULL, del);
 		}
 		av[i] = NULL;
+		execmd(av);
 
-		child = fork();
-		if (child == -1)
+		for (i = 0; i < splitcount - 1; 1++)
 		{
-			perror("fork failed");
-			exit(EXIT_FAILURE);
-		}
-		if (child == 0)
-		{
-			execvp(av[0], av);
-			perror("command does not exist");
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			waitpid(child, &status, 0);
+			free(av[i]);
 		}
 		free(av);
+		free(buffer2);
+		free(buffer);
+		buffer = NULL;
+		n = 0;
+		splitcount = 0;
 	}
-
-	free(buffer);
-	return (0);
+	return (EXIT_SUCCESS);
+}
+/**
+ * execmd - A function which gets the env
+ * @av: The argument
+ */
+void execmd(char **av)
+{
+	char *md = NULL;
+	if (av)
+	{
+		md = av[0];
+		if (execve(md, av, NULL) == -1)
+		{
+			printf("%s: command not found\n", md);
+		}
+	}
 }
 
