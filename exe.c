35310ae38055f5e24aef0a25fac245780dcf2347
cartermusee <cartermusee@gmail.com>
2023-04-28 00:37:05 -0700

    changes

--- exe.c
+++ exe.c
@@ -4,28 +4,16 @@
  * execmd - A function which gets the env
  * @av: The argument
  */
-
 void execmd(char **av)
 {
-    pid_t pid;
-    int status;
+	char *md = NULL;
 
-    pid = fork();
-    if (pid < 0)
-    {
-        perror("fork error");
-        exit(1);
-    }
-    if (pid == 0)
-    {
-        if (execve(av[0], av, NULL) == -1)
-        {
-            perror("execve error");
-            exit(1);
-        }
-    }
-    else
-    {
-        wait(&status);
-    }
+	if (av)
+	{
+		md = av[0];
+		if (execve(md, av, NULL) == -1)
+		{
+			perror("invalid");
+		}
+	}
 }
